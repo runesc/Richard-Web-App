bbabd1c9a2ba942afd3964307a8264dee2195b93
Luis Alfredo Reyes <130002495+zapata-97@users.noreply.github.com>
2024-03-01 17:12:12 -0600

    cambios en preview

--- src/pages/preview/index.jsx
+++ src/pages/preview/index.jsx
@@ -1,7 +1,7 @@
 // read query params from url
 import { useParams } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
-import numeroALetras from '../../utils/numbers'
+import numeroALetras from "../../utils/numbers";
 import axios from "axios";
 import "./preview.styles.css";
 import DatePicker from "react-datepicker";
@@ -14,8 +14,16 @@ const Preview = () => {
   const { contract, invoice } = useParams();
   const [data, setData] = useState({});
   const [startDate, setStartDate] = useState(new Date());
-  const [amount, setAmount] = useState(2500.00)
-  const reference = useRef(null)
+  const [amount, setAmount] = useState(2500.0);
+  const reference = useRef(null);
+  const [tempData, setTempData] = useState({});
+
+  const handleTempChange = (e) => {
+    setTempData({
+      ...tempData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -30,6 +38,12 @@ const Preview = () => {
         })
         .then((res) => {
           setData(res.data.invoice);
+          setTempData({
+            ...res.data.invoice,
+            name: `${res.data.invoice.name} ${res.data.invoice.lastName}`,
+            loc: `PAGO DE TERRENO(S) EJIDO BENITO JUÁREZ, MANZANA ${res.data.invoice.manzana} LOTE ${res.data.invoice.lote}`,
+            owner: "Julia García Cruz",
+          });
         })
         .catch((err) => {
           console.log(err);
@@ -40,20 +54,28 @@ const Preview = () => {
   }, []);
 
   function obtenerSoloNumeros(cadena) {
-    if(typeof cadena === "number"){
-      return cadena
+    if (typeof cadena === "number") {
+      return cadena;
     }
 
     const regex = /[^0-9$]/g;
-    let numeros = cadena.replace(regex, '').replace(',', '').replace('$', '');
-    numeros = numeros.substr(0,numeros.length-2)
-    setAmount(parseFloat(numeros))
+    let numeros = cadena.replace(regex, "").replace(",", "").replace("$", "");
+    numeros = numeros.substr(0, numeros.length - 2);
+    setAmount(parseFloat(numeros));
   }
 
   return (
     <div className="invoice p-5 w-100">
       <div className="franja d-flex">
-        <div className="numero_pago">No. {invoice}</div>
+        <div className="numero_pago">
+          No.{" "}
+          <input
+            type="text"
+            value={tempData.numeroDePago}
+            name="numeroDePago"
+            onChange={handleTempChange}
+          />
+        </div>
         <div className="direccion_enrique_segoviano ms-auto text-end">
           <span className="d-block">Calle Hermanos Vázquez Gómez #251-C</span>
           <span className="d-block">Zona centro cp. 87000</span>
@@ -77,9 +99,7 @@ const Preview = () => {
           <input
             type="text"
             defaultValue={"$2,500.00 MN"}
-            onChange={
-              (e)=> setAmount(e.target.value)
-            }
+            onChange={(e) => setAmount(e.target.value)}
             ref={reference}
             className="form-control hide__print__border ms-auto limited__width"
           />
@@ -89,38 +109,48 @@ const Preview = () => {
       <div className="bordered__container mt-3">
         <p className="client__name p-3 pb-0">
           <b>
-            {data.name} {data.lastName}
+            <input
+              type="text"
+              name="name"
+              value={`${tempData.name}`}
+              onChange={handleTempChange}
+            />
           </b>
         </p>
 
         <div className="degraded__band">
           <b>Por la cantidad de:</b>{" "}
           <span className="quantity__text">
-            {
-              numeroALetras(obtenerSoloNumeros(amount), {
-                plural: "PESOS",
-                singular: "PESOS",
-                centPlural: "",
-                centSingular: ""
-              })
-            }
-           {
-            reference.current ? <span>({reference.current.value})</span> : <div>($2,500.00 MN)</div>
-           }
+            {numeroALetras(obtenerSoloNumeros(amount), {
+              plural: "PESOS",
+              singular: "PESOS",
+              centPlural: "",
+              centSingular: "",
+            })}
+            {reference.current ? (
+              <span>({reference.current.value})</span>
+            ) : (
+              <div>($2,500.00 MN)</div>
+            )}
           </span>
         </div>
 
-        <div className="concept">
+        <div className="concept d-flex gap-2 align-items-center pb-0 pt-3">
           <b>Concepto:</b>{" "}
-          <span className="text-decoration-underline">
-            PAGO DE TERRENO(S) EJIDO BENITO JUÁREZ, MANZANA {data.manzana} LOTE{" "}
-            {data.lote}
+          <span className="text-decoration-underline d-block w-100">
+            <input
+              type="text"
+              className="location__input"
+              name="loc"
+              value={tempData.loc}
+              onChange={handleTempChange}
+            />
           </span>
-          <span className="d-block separator"></span>
         </div>
+        <span className="d-block separator"></span>
         <div className="sign mb-3">
           <hr />
-          Julia García Cruz
+          <input type="text" className="owner__input" name="owner" value={tempData.owner} onChange={handleTempChange} />
         </div>
       </div>
     </div>
